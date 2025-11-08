13f02b9b26fd126f3abbfbe3249c21b774f8741e
MansurSharazoori <Xpert.Web.911@gmail.com>
2025-11-08 18:17:54 +0300

    the footer is pass

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,3 @@
-const menu = document.querySelector(".menu");
-const list = document.querySelector(".list");
-// const sidebar = document.querySelector(".side-bar");
-const item = document.querySelectorAll(".item");
-const container = document.querySelector(".main-container");
-
-item.forEach((i) => {
-  i.addEventListener("click", function () {
-    sidebar.classList.remove("open");
-  });
-});
+// this is for the footer and its copy right age only
+const age = document.querySelector(".thedate");
+age.textContent = new Date().getFullYear();
