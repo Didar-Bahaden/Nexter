5d2790c40ee4d745b22f8d223989154d7ab4822a
MansurSharazoori <Xpert.Web.911@gmail.com>
2025-11-12 19:07:28 +0300

    the drop menu pass and finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 // this is for the footer and its copy right age only
 const age = document.querySelector(".thedate");
 age.textContent = new Date().getFullYear();
+
+// drop menu button arrangement
+
+const target = document.querySelector(".main-container");
+const button = document.querySelector(".menu");
+
+button.addEventListener("click", function () {
+  target.classList.toggle("open");
+});
