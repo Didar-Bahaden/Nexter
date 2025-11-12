4726f46bc9d5dd1eb57a958f801fbb16df4e1855
MansurSharazoori <Xpert.Web.911@gmail.com>
2025-11-12 19:17:23 +0300

    new update

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,16 @@ age.textContent = new Date().getFullYear();
 
 const target = document.querySelector(".main-container");
 const button = document.querySelector(".menu");
+const all = document.querySelectorAll(".x12");
 
 button.addEventListener("click", function () {
   target.classList.toggle("open");
+});
+
+// to hide the list of drop menu while you click one of the iteam in tghe list
+all.forEach(function (link) {
+  link.addEventListener("click", function () {
+    console.log(link);
+    target.classList.toggle("open");
+  });
 });
